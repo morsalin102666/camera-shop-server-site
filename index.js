f2f795104e048ec55a7cc2e13ca105bb4334be0c
Morsalin <mdmorsalinislam027@gmail.com>
2023-05-20 01:08:35 +0600

    delet post code create

--- index.js
+++ index.js
@@ -77,6 +77,14 @@ async function run() {
       res.send(result)
     })
 
+    // --------- delet post -----------
+    app.delete('/cameras/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await cameraShop.deleteOne(query)
+      res.send(result)
+    })
+
 
     // ================== client site handel start ===================
 
